a5579403ec8886f198120a41f49221fe0a38f7f1
Viktoria-Kiyashko <dzekuajld@gmail.com>
2024-11-07 05:02:36 +0300

    Update index.ts

--- src/App/Components/ProductItem/index.ts
+++ src/App/Components/ProductItem/index.ts
@@ -1,7 +1,6 @@
 
-//index.ts
-// src/App/Components/ProductItem/index.ts
+
 export {};
 
-// Или, если у вас есть экспорт из этого файла:
-export * from './ProductItem'; // Если хотите экспортировать все, что есть в ProductItem.ts
+
+export * from './ProductItem'; 
